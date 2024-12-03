e85e9c6a635e1c2dfbda40ac8dcaeba028b7dfbc
Jules Charlier <jules.charlier123@gmail.com>
2024-12-03 09:49:57 +0100

    ex 13

--- ex_12.py
+++ ex_12.py
@@ -1,14 +1,14 @@
 def egal(f, g):
     return f[0] * g[1] == g[0] * f[1]
 
-def pgcd(a, b):
-    p, r = a, b
+def pgcd(f):
+    p, r = f[0], f[1]
     while r != 0:
         p, r = r, p % r
     return p
 
 def simpl(f):
-    return [f[0] // pgcd(f[0], f[1]), f[1] // pgcd(f[0], f[1])]
+    return [f[0] // pgcd(f), f[1] // pgcd(f)]
 
 def inverse(f):
     return [f[1], f[0]]
---
+++ ex_13.py
@@ -0,0 +1,4 @@
+def egypt(a, b):
+    a, b = min(a, b), max(a, b)
+    p = int(a/b)
+    
