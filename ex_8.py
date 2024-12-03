a6ac0bf34b3d953508fe09f8a999b419b8d08a65
Jules Charlier <jules.charlier123@gmail.com>
2024-12-03 08:42:33 +0100

    tryc

--- ex_8.py
+++ ex_8.py
@@ -10,4 +10,7 @@ def truc():
 
 truc()
 
+print([i**3 + j**3 + k**3 + l**3 for i in range(10) for j in range(10) for k in range(10) for l in range(10) if i**3 + j**3 + k**3 + l**3 == 1000*i + 100*j + 10*k + l])
+
+
 # 0 1 153 370 371 407
